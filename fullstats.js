9ebec423f43594264e813287ec6d833d627f8eb7
Daniel Kernicky <danielkernicky@Daniels-MacBook-Pro.local>
2016-12-08 19:07:25 -0500

    So many stats

--- fullstats.js
+++ fullstats.js
@@ -54,11 +54,45 @@ function getCharacterActivities(activities, membershipType, membershipId, charac
             });
             return Promise.all(promises)
             .then(activities => {
-                logger.trace(activities.length + ':)');
+                // logger.trace(activities.length + ':)');
                 //logger.trace(activities);
+                return reduce(activities)
             });
         }
     });
+}
+
+function reduce(activities) {
+    activities = activities.reduce((a, b) => {
+        let spree = Math.max(a.longestKillSpree, b.longestKillSpree);
+        let life = Math.max(a.longestSingleLife, b.longestSingleLife);
+        let medals = {};
+        for (aMedals in a.medals) {
+            if (b.medals[aMedals]) {
+                medals[aMedals] = a.medals[aMedals] + b.medals[aMedals];
+            } else {
+                medals[aMedals] = a.medals[aMedals];
+            }
+        }
+        for (bMedals in b.medals) {
+            if (!a.medals[bMedals]) {
+                medals[bMedals] = b.medals[bMedals];
+            }
+        }
+        return {
+            kills: a.kills + b.kills,
+            deaths: a.deaths + b.deaths,
+            score: a.score + b.score,
+            precisionKills: a.precisionKills + b.precisionKills,
+            longestKillSpree: spree,
+            longestSingleLife: life,
+            combatRating: a.combatRating + b.combatRating,
+            place: a.place + b.place,
+            games: a.games + b.games,
+            medals: medals
+        }
+    })
+    return Promise.resolve(activities)
 }
 
 function addPostGameCarnage(characterId, activity, activityId) {
@@ -73,27 +107,114 @@ function addPostGameCarnage(characterId, activity, activityId) {
         });
     })
     .then(result => {
+
         return getPostGameAdvancedMetrics(characterId, result);
     });
 }
 
 function getPostGameAdvancedMetrics(characterId, activity) {
-    // medals = activity.entry[n].extended.values
-    // weapons = activity.entry[n].extended.Weapons
+    // medals = activity.entries[n].extended.values
+    // weapons = activity.entries[n].extended.Weapons
+    let promises = [];
+    promises.push(getPlace(characterId, activity));
+    promises.push(getOtherStats(characterId, activity));
+    return Promise.all(promises)
+    .then(result => {
+        result[1].place = result[0];
+        return Promise.resolve(result[1]);
+    })
+    // .then(result => {
+    //     // kills: 14,
+    //     // deaths: 13,
+    //     // score: 2750,
+    //     // precisionKills: 9,
+    //     // longestKillSpree: 5,
+    //     // longestSingleLife: 90,
+    //     // combatRating: 103.57954114397154,
+    //     // weaponBestType: 'Sniper Rifle',
+    //     // medals: [Object],
+    //     // place: 2
+    //
+    //     logger.info(result)
+    //     return Promise.resolve(result.reduce((a, b) => {
+    //         let spree = a.longestKillSpree < b.longestKillSpree ? a.longestKillSpree : b.longestKillSpree;
+    //         let life = Math.max(a.longestSingleLife, b.longestSingleLife);
+    //         return {
+    //             kills: a.kills + b.kills,
+    //             deaths: a.deaths + b.deaths,
+    //             score: a.score + b.score,
+    //             precisionKills: a.precisionKills + b.precisionKills,
+    //             longestKillSpree: spree,
+    //             longestSingleLife: life,
+    //             combatRating: a.combatRating + b.combatRating,
+    //             place: a.place + b.place
+    //         }
+    //     }));
+    // });
+}
 
-    // if (activity.entries.length > 6) {
-    //     logger.error('**************WTF?**************');
-    //     //logger.debug(activity.entries)
-    //     activity.entries.forEach(entry => {
-    //         logger.debug(entry.values.completed);
-    //     })
-    //     logger.error('**************WTF?**************');
-    // }
-    return getPlace(characterId, activity);
+function analyzePerCharacterStats(activities) {
+    //logger.info(data);
+    logger.info(activities);
+}
+
+function getOtherStats(characterId, activity) {
+    //logger.info(activity.entries)
+    let values = activity.entries.find(entry => {
+        return entry.characterId === characterId;
+    }).extended.values;
+    //logger.debug(values);
+    let stats = {
+        // totalKillDistance: values.totalKillDistance.basic.value || null,
+        // kills: values.kills.basic.value,
+        // deaths: values.deaths.basic.value,
+        // score: values.score.basic.value,
+        // precisionKills: values.precisionKills.basic.value,
+        // longestKillSpree: values.longestKillSpree.basic.value,
+        // longestSingleLife: values.longestSingleLife.basic.value, //seconds
+        // combatRating: values.combatRating.basic.value,
+        // weaponBestType: values.weaponBestType.basic.displayValue,
+        //weaponKillsSuper: val.extended.values.weaponKillsSuper.basic.displayValue,
+
+        totalKillDistance: '',
+        kills: '',
+        deaths: '',
+        score: '',
+        precisionKills: '',
+        longestKillSpree: '',
+        longestSingleLife: '', //seconds
+        combatRating: '',
+        weaponBestType: ''
+
+    }
+    for (key in stats) {
+        if (key === 'weaponBestType') {
+            stats[key] = values[key] ? values[key].basic.displayValue : null;
+        } else {
+            stats[key] = values[key] ? values[key].basic.value : null;
+        }
+    }
+    stats.games = 1;
+    stats.medals = {};
+
+    for (key in values) {
+        //logger.warn('BEFORE' + item);
+        if (key.indexOf('medal') !== -1) {
+            stats.medals[key] = values[key].basic.value;
+        }
+    }
+    // logger.error('AFTER');
+    // logger.error(medals);
+    //logger.error(stats);
+    return Promise.resolve(stats);
+    //logger.info(val.extended.values)
+}
+
+function getWeapons(characterId, activity) {
 }
 
 function getPlace(characterId, activity) {
-    Promise.resolve(activity.entries.sort((a, b) => {
+    return Promise.resolve(activity.entries.sort((a, b) => {
         if (a.score.basic.value > b.score.basic.value) {
             return -1;
         } else if (a.score.basic.value < b.score.basic.value) {
@@ -153,6 +274,7 @@ function entry() {
         return getActivityHistory(result, mode);
     })
     .then(result => {
+        return analyzePerCharacterStats(result);
         //logger.warn(result);
         //logger.debug(result[0][0]);
         //logger.debug(result[0][3].values.standing);
